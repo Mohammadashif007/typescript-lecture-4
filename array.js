81e2243691f347eaa106019d533cf341ed8fb59a
Mohammad Ashif <Mohammad Ashif>
2024-04-24 09:10:24 +0600

    typescript array of type

---
+++ array.js
@@ -0,0 +1,3 @@
+var numbers = [2, 33, 44, 55, 66];
+numbers.push(99);
+console.log(numbers);
---
+++ array.ts
@@ -0,0 +1,16 @@
+const numbers = [2, 33, 44, 55, 66];
+numbers.push(99);
+// console.log(numbers);
+// numbers[1] = 'new';
+// numbers.push('roy')
+
+const str = ['mosh', 'john', 'tommy'];
+// str.push(44)
+// str.push(true)
+str.push('cruse');
+
+const mixed = [1, 'tom', 4, 'jerry'];
+
+mixed.push(2);
+mixed.push('biden');
+// mixed.push(false)
