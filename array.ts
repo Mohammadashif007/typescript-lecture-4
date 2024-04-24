e47fe98786a72db9825d121518515ab231f25f84
Mohammad Ashif <Mohammad Ashif>
2024-04-24 09:31:19 +0600

    typescript object is added

--- array.ts
+++ array.ts
@@ -1,3 +1,5 @@
+// ! array
+
 const numbers = [2, 33, 44, 55, 66];
 numbers.push(99);
 // console.log(numbers);
@@ -9,8 +11,35 @@ const str = ['mosh', 'john', 'tommy'];
 // str.push(true)
 str.push('cruse');
 
-const mixed = [1, 'tom', 4, 'jerry'];
+ const mixed = [1, 'tom', 4, 'jerry']; //! we cant change the variable type
+
+//!   mixed = 'tom'; we can't do this 
 
 mixed.push(2);
 mixed.push('biden');
 // mixed.push(false)
+
+
+// ! Object
+
+//! we can't change the type of variable as well as we can't change the type of property value 
+
+let ninja = {     
+    name: 'mario',
+    belt: 'black',
+    age: 30
+}
+
+ninja.name = 'ryo';
+ninja.age = 44;
+// ninja.age = '33' //! we can't do that because object property will be same when it was initialized
+
+// ninja.skills = ['fighting', 'sneaking'] //! we can't do that because in typescript object we can't put extra property which was not initialized
+
+ninja = {
+    name: 'john',
+    belt: 'silver',
+    age: 33
+} 
+
+//! we can update the object to maintain same structure but we can't change the property structure, we can change the value
